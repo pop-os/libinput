1ad2faf693fc5565d6ef2ccbc4dc11f46c8681cf
Emilio Pozuelo Monfort <pochu@debian.org>
2014-09-12 19:17:35 +0200

    Imported Upstream version 0.6.0+dfsg

--- test/litest-generic-singletouch.c
+++ test/litest-generic-singletouch.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013 Red Hat, Inc.
+ * Copyright © 2014 Red Hat, Inc.
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
@@ -27,20 +27,17 @@
 #include "litest.h"
 #include "litest-int.h"
 
-static void litest_bcm5974_setup(void)
+static void
+litest_generic_singletouch_touch_setup(void)
 {
-	struct litest_device *d = litest_create_device(LITEST_BCM5974);
+	struct litest_device *d = litest_create_device(LITEST_GENERIC_SINGLETOUCH);
 	litest_set_current_device(d);
 }
 
-struct input_event down[] = {
-	{ .type = EV_ABS, .code = ABS_X, .value = LITEST_AUTO_ASSIGN  },
+static struct input_event down[] = {
+	{ .type = EV_ABS, .code = ABS_X, .value = LITEST_AUTO_ASSIGN },
 	{ .type = EV_ABS, .code = ABS_Y, .value = LITEST_AUTO_ASSIGN },
-	{ .type = EV_ABS, .code = ABS_PRESSURE, .value = 30  },
-	{ .type = EV_ABS, .code = ABS_MT_SLOT, .value = LITEST_AUTO_ASSIGN },
-	{ .type = EV_ABS, .code = ABS_MT_TRACKING_ID, .value = LITEST_AUTO_ASSIGN },
-	{ .type = EV_ABS, .code = ABS_MT_POSITION_X, .value = LITEST_AUTO_ASSIGN },
-	{ .type = EV_ABS, .code = ABS_MT_POSITION_Y, .value = LITEST_AUTO_ASSIGN },
+	{ .type = EV_KEY, .code = BTN_TOUCH, .value = 1 },
 	{ .type = EV_SYN, .code = SYN_REPORT, .value = 0 },
 	{ .type = -1, .code = -1 },
 };
@@ -49,8 +46,7 @@ static struct input_event move[] = {
 	{ .type = EV_ABS, .code = ABS_MT_SLOT, .value = LITEST_AUTO_ASSIGN },
 	{ .type = EV_ABS, .code = ABS_X, .value = LITEST_AUTO_ASSIGN },
 	{ .type = EV_ABS, .code = ABS_Y, .value = LITEST_AUTO_ASSIGN },
-	{ .type = EV_ABS, .code = ABS_MT_POSITION_X, .value = LITEST_AUTO_ASSIGN },
-	{ .type = EV_ABS, .code = ABS_MT_POSITION_Y, .value = LITEST_AUTO_ASSIGN },
+	{ .type = EV_KEY, .code = BTN_TOUCH, .value = 1 },
 	{ .type = EV_SYN, .code = SYN_REPORT, .value = 0 },
 	{ .type = -1, .code = -1 },
 };
@@ -61,50 +57,31 @@ static struct litest_device_interface interface = {
 };
 
 static struct input_absinfo absinfo[] = {
-	{ ABS_X, -4824, 4824, 0, 0, 0 },
-	{ ABS_Y, -172, 4290, 0, 0, 0 },
-	{ ABS_PRESSURE, 0, 256, 5, 0, 0 },
-	{ ABS_TOOL_WIDTH, 0, 16, 0, 0, 0 },
-	{ ABS_MT_SLOT, 0, 15, 0, 0, 0 },
-	{ ABS_MT_POSITION_X, -4824, 4824, 17, 0, 0 },
-	{ ABS_MT_POSITION_Y, -172, 4290, 17, 0, 0 },
-	{ ABS_MT_ORIENTATION, -16384, 16384, 3276, 0, 0 },
-	{ ABS_MT_TOUCH_MAJOR, 0, 2048, 81, 0, 0 },
-	{ ABS_MT_TOUCH_MINOR, 0, 2048, 81, 0, 0 },
-	{ ABS_MT_WIDTH_MAJOR, 0, 2048, 81, 0, 0 },
-	{ ABS_MT_WIDTH_MINOR, 0, 2048, 81, 0, 0 },
-	{ ABS_MT_TRACKING_ID, 0, 65535, 0, 0, 0 },
-	{ ABS_MT_PRESSURE, 0, 255, 0, 0, 0 },
+	{ ABS_X, 10000, 20000, 0, 0, 10 },
+	{ ABS_Y, -2000, 2000, 0, 0, 9 },
 	{ .value = -1 },
 };
 
 static struct input_id input_id = {
-	.bustype = 0x3,
-	.vendor = 0x5ac,
-	.product = 0x249,
+	.bustype = 0x01,
+	.vendor = 0x02,
+	.product = 0x03,
 };
 
 static int events[] = {
-	EV_KEY, BTN_LEFT,
-	EV_KEY, BTN_TOOL_FINGER,
-	EV_KEY, BTN_TOOL_QUINTTAP,
 	EV_KEY, BTN_TOUCH,
-	EV_KEY, BTN_TOOL_DOUBLETAP,
-	EV_KEY, BTN_TOOL_TRIPLETAP,
-	EV_KEY, BTN_TOOL_QUADTAP,
-	INPUT_PROP_MAX, INPUT_PROP_BUTTONPAD,
-	-1, -1
+	INPUT_PROP_MAX, INPUT_PROP_DIRECT,
+	-1, -1,
 };
 
-struct litest_test_device litest_bcm5974_device = {
-	.type = LITEST_BCM5974,
-	.features = LITEST_TOUCHPAD | LITEST_CLICKPAD |
-		    LITEST_BUTTON | LITEST_APPLE_CLICKPAD,
-	.shortname = "bcm5974",
-	.setup = litest_bcm5974_setup,
+struct litest_test_device litest_generic_singletouch_device = {
+	.type = LITEST_GENERIC_SINGLETOUCH,
+	.features = LITEST_SINGLE_TOUCH,
+	.shortname = "generic-singletouch",
+	.setup = litest_generic_singletouch_touch_setup,
 	.interface = &interface,
 
-	.name = "bcm5974",
+	.name = "generic_singletouch",
 	.id = &input_id,
 	.events = events,
 	.absinfo = absinfo,
